32861b8b3a90202d5527d43b514ab44af28a25c0
Chris <Chris@visibleone.com>
2025-06-11 22:48:59 +0630

    react forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import List from "./List.jsx";
 import Item from "./Item.jsx";
+import Form from "./Form.jsx";
 import { useState } from "react";
+
 
 export default function App() {
   const [data, setData] = useState([
@@ -9,6 +11,12 @@ export default function App() {
     { id: 3, content: "Yay, interesting.", name: "Chris" },
   ]);
 
+  const [showForm, setShowForm] = useState(false);
+
+  const add = (content, name) => {
+    const id = data[data.length - 1].id + 1;
+    setData([...data, { id, content, name }]);
+  }
 
   const remove = (id) => {
     let filteredData = data.filter(item => item.id !== id);
@@ -17,7 +25,28 @@ export default function App() {
 
   return (
     <div style={{ maxWidth: 600, margin: "20px auto" }}>
-      <h1>Yaycha</h1>
+      <div style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignContent: "center",
+        alignItems: "center"
+      }}>
+        <h1>Yaycha</h1>
+
+        <button
+          onClick={() => setShowForm(!showForm)}
+          style={{
+            borderRadius: 50,
+            border: "0 none",
+            background: showForm ? "#dc3545" : "#0d6efd",
+            color: "white",
+          }}>
+          {showForm ? "×" : "+"}
+        </button>
+      </div>
+
+      { showForm && <Form add={add} /> }
+
       <List>
         {
           data.map(item => <Item key={item.id} item={item} remove={remove} />)
